84406d6a554c8949870ae1b542f45f9a216c1eac
lcslima45 <lcslima45@gmail.com>
2020-07-28 02:13:37 -0300

    Comentários

--- pointer.go
+++ pointer.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var example int     //name of variables tend to be written as nouns
-	example = 1         //example is int type by implicity
+	var example int     //names of variables tend to be written as nouns
+	example = 1         //example is int type by implicity attribution
 	pointer := &example //pointer is inialized as the memory address that points where example store the content that is 1 now
 	fmt.Println(example, *pointer)
 	*pointer = 2
 	fmt.Println(example, *pointer) //exit 2 2, as example and *pointer (that is the content of the address stored in pointer) go to the same content 2
-	doSomething(&example)
-	fmt.Println(example, *pointer)
+	doSomething(&example)          //now that you passed the reference of the variable to the function, the content stored has changed
+	fmt.Println(example, *pointer) //now example and *pointer will get exit 3 3
 }
 
-//name of functions tend to be written as verbs
+//names of functions tend to be written as verbs
 func doSomething(enter *int) {
 	*enter = 3
 	return
